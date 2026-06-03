44b89784586482e1121aabe494e1e9ec397b51f5
André Oliveira <131814071+AndreOliveira509@users.noreply.github.com>
2026-06-03 12:39:32 -0300

    .

---
+++ JAVA AI BACKEND/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Configurando a JDK na Versão " + args[0]);
+        System.out.println("Programando em " + args[1]);
+        
+    }
+}
