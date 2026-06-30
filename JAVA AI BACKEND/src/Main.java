b30299d0d2235df957d4cfce69d978226a114a25
André Oliveira <131814071+AndreOliveira509@users.noreply.github.com>
2026-06-30 13:27:36 -0300

    Add files via upload

--- JAVA AI BACKEND/src/Main.java
+++ JAVA AI BACKEND/src/Main.java
@@ -1,16 +1,12 @@
-import java.util.Scanner;  
-
 public class Main {
    public static void main(String[] args) {
-        int value1 = 6;
-        String binaryValue = Integer.toBinaryString(value1);
-        int value2 = 2;
-        String binaryValue2 = Integer.toBinaryString(value2);
-        System.out.printf("Primeiro valor: %d (binário: %s)\n", value1, binaryValue);
-        System.out.printf("Segundo valor: %d (binário: %s)\n", value2, binaryValue2);
-        int result = value1 >> value2;
-        String binaryResult = Integer.toBinaryString(result);
-        System.out.printf("Resultado do AND: %s >> %s = %s (binário: %s)\n", binaryValue, binaryValue2, result, binaryResult);
+       
+       Person male = new Person("Andre"); 
+       
+       Person female = new Person("Maria");
+      male.incAge();
+       System.out.println("Homem: " + male.getName() + ", Idade: " + male.getAge());
+       System.out.println("Mulher: " + female.getName() + ", Idade: " + female.getAge());
    }
 }
     
